57df1ccbcafa23f6d956bb85449c6ad51b7c0b9c
ohnowho <ohnowho@outlook.com>
2026-07-04 21:52:34 +0400

    Initial Commit

---
+++ ohnowho-appTests/ohnowho_appTests.swift
@@ -0,0 +1,18 @@
+//
+//  ohnowho_appTests.swift
+//  ohnowho-appTests
+//
+//  Created by konan on 7/4/26.
+//
+
+import Testing
+
+struct ohnowho_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
